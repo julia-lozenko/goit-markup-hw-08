cf250d7408e72e15f4293dc0fb7c229f1699a411
Julia Lozenko <jlozenko@gmail.com>
2021-01-25 19:51:40 +0200

    mob-menu

--- js/modal.js
+++ js/modal.js
@@ -1,28 +1,15 @@
-(() => {
-  // refs.modal.classList.toggle("is-hidden");
 
+(() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+    refs.modal.classList.toggle('is-hidden');
   }
-})();
-
-(() => {
-  const menuBtnRef = document.querySelector("[data-menu-btn]");
-  const mobileMenuRef = document.querySelector("[data-mob-menu]");
-  menuBtnRef.addEventListener("click", () => {
-    const expanded =
-      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-    menuBtnRef.classList.toggle("is-open");
-    menuBtnRef.setAttribute("aria-expanded", !expanded);
-    mobileMenuRef.classList.toggle("is-open");
-  });
 })();
